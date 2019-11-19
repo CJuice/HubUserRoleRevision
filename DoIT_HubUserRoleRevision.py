e164c6b076cf0a2506e4a305a336ec1f35b2c3c1
CJuice <CJuice@users.noreply.github.com>
2019-11-19 10:06:12 -0500

    Update DoIT_HubUserRoleRevision.py

--- DoIT_HubUserRoleRevision.py
+++ DoIT_HubUserRoleRevision.py
@@ -1,4 +1,6 @@
 """
+Manipulate Hub Community with Python API.
+Used originally to manipulate the role of users in the community to make it possible for them to Join External Groups
 
 """
 
@@ -18,12 +20,15 @@ def main():
     md_hub_pwd = config_parser["DEFAULT"]["password"]
     maryland_viewer_key = "EuJRbh4M3lBwBRI8"
     esri_viewer_key = "iAAAAAAAAAAAAAAA"
+    esri_user_key = "org_user"
 
     # FUNCTIONS
 
     # FUNCTIONALITY
     gis = GIS(url=md_hub_url, username=md_hub_admin, password=md_hub_pwd)
     users = gis.users
+
+    print("\nBefore manipulation:")
     user_role_df = users.counts(type="role", as_df=True)
     print(user_role_df)
 
@@ -33,13 +38,16 @@ def main():
     # print(users.search(role="EuJRbh4M3lBwBRI8"))  # MarylandViewer role key
 
     # Manipulate the role of community accounts
-    if False:
-        viewer_role_users = users.search(role=esri_viewer_key)
+    if True:
+        viewer_role_users = users.search(role=esri_user_key)
         for viewer in viewer_role_users:
             print(f"Viewer: {viewer.fullName}, Role: {viewer.role}")
-            # result = viewer.update_role(role=maryland_viewer_key)
+            result = viewer.update_role(role=maryland_viewer_key)
             print(f"\tModified: {result}")
 
+    print("\nAfter manipulation:")
+    user_role_df = users.counts(type="role", as_df=True)
+    print(user_role_df)
 
 if __name__ == "__main__":
     main()
