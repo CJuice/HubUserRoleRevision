4f96981e446293476d474c6e299b0913b9753381
CJuice <CJuice@users.noreply.github.com>
2019-11-19 09:48:35 -0500

    Update DoIT_HubUserRoleRevision.py

--- DoIT_HubUserRoleRevision.py
+++ DoIT_HubUserRoleRevision.py
@@ -4,6 +4,7 @@
 
 
 def main():
+
     # IMPORTS
     from arcgis.gis import GIS
     import configparser
@@ -15,7 +16,8 @@ def main():
     md_hub_url = config_parser["DEFAULT"]["url"]
     md_hub_admin = config_parser["DEFAULT"]["login"]
     md_hub_pwd = config_parser["DEFAULT"]["password"]
-    role_search = """{role: 'org_admin'"""
+    maryland_viewer_key = "EuJRbh4M3lBwBRI8"
+    esri_viewer_key = "iAAAAAAAAAAAAAAA"
 
     # FUNCTIONS
 
@@ -26,10 +28,17 @@ def main():
     print(user_role_df)
 
     # print(users.search(role="org_admin"))
-    # print(users.search(role="iAAAAAAAAAAAAAAA")) # Viewer role, no idea why it is this string of characters
+    # print(users.search(role="iAAAAAAAAAAAAAAA")) # Viewer role key
     # print(users.search(role="org_publisher"))
-    print(users.search(role="EuJRbh4M3lBwBRI8"))  # MarylandViewer role
-    pass
+    # print(users.search(role="EuJRbh4M3lBwBRI8"))  # MarylandViewer role key
+
+    # Manipulate the role of community accounts
+    if False:
+        viewer_role_users = users.search(role=esri_viewer_key)
+        for viewer in viewer_role_users:
+            print(f"Viewer: {viewer.fullName}, Role: {viewer.role}")
+            # result = viewer.update_role(role=maryland_viewer_key)
+            print(f"\tModified: {result}")
 
 
 if __name__ == "__main__":
